94a3568e8816de0c02faeccb20079f87176b4b29
mj120402 <justmo12@hotmail.com>
2022-08-25 18:56:13 +0100

    variables and syntaxes in js

---
+++ syntax.js
@@ -0,0 +1,21 @@
+//Here is using variables in Javascript
+
+var msg = 'Hello World';
+console.log(msg);
+
+// const variable shown here
+const text= "goodbye world";
+
+// re-initialised variables
+var num= "5"
+console.log(num);
+num= num-3
+console.log(num);
+
+//undefined vairable shown here
+var x='undefined'
+console.log(x);
+
+// null variable shown here
+var y='null'
+console.log(y);
